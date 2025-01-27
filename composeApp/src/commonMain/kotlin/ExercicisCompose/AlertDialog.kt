4477146c4eb9e773d70dad9a60308894175da640
miquel-manzano <miquel.manzano.7e6@itb.cat>
2025-01-27 12:57:05 +0100

    Testing

--- composeApp/src/commonMain/kotlin/ExercicisCompose/AlertDialog.kt
+++ composeApp/src/commonMain/kotlin/ExercicisCompose/AlertDialog.kt
@@ -10,7 +10,14 @@ import androidx.compose.runtime.*
 data class Message(val text: String)
 @Composable
 fun AlertDialog() {
+
     Column {
+        var text by remember { mutableStateOf("") }
+        TextField(text,
+            label = { Text("write here") },
+            onValueChange = {
+                text = it
+            })
         var showDialog by remember { mutableStateOf(false) }
         if(showDialog)
             AlertDialog(
